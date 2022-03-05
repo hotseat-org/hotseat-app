bfb0a24850b1110a7f0b1c24ca77a4711343457c
Josef Vidlák <vidlec@gmail.com>
2022-03-05 22:49:14 +0100

    feat(css): adds tailwind.css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,14 +7,24 @@ export const { loader, useData } = dataLoader(async () => {
   return allUsers.map((user) => exclude(user, 'PK'))
 })
 
+export const handle = {
+  foo: 1,
+}
+
 export default function Index() {
   const users = useData()
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
+            key={user.id}
+          >
+            {user.name}
+          </li>
         ))}
       </ul>
     </div>
--- app/utils/remix.ts
+++ app/utils/remix.ts
@@ -1,8 +1,5 @@
 import { useLoaderData } from 'remix'
 
-export type LoaderReturnType<Loader extends (...args: unknown[]) => unknown> =
-  Awaited<ReturnType<Loader>>
-
 export const dataLoader = <Data extends unknown>(
   loader: () => Promise<Data>
 ) => ({
