901646b31a1c5e4d1273615842cd67fb2b4fe0c0
Jitka Černohorská <cernojit@gmail.com>
2022-03-12 14:05:38 +0100

    feat(header): header date

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,12 @@
-import { Form, LoaderFunction, useLoaderData } from 'remix'
-
+import { Form, LinksFunction, LoaderFunction, useLoaderData } from 'remix'
+import Header from '~/components/Header/index.js'
 import { authenticator } from '../services/auth.server.js'
+import headerStyle from '../styles/header.css'
 
+
+export let links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: headerStyle }]
+}
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await authenticator.isAuthenticated(request, {
     failureRedirect: '/login',
@@ -19,6 +24,7 @@ const Dashboard = () => {
 
   return (
     <div className="w-full flex p-4 bg-red-500 items-center justify-end gap-8">
+      <Header/>
       <h1>{user.displayName}</h1>
       <Form method="post" action={`/logout`}>
         <button className="p-2 bg-slate-500 hover:bg-slate-600 rounded transition">
