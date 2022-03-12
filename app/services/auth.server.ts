7a410b13527502447f613fbf76c24efff8f278cb
Josef Vidlák <vidlec@gmail.com>
2022-03-12 12:11:05 +0100

    feat(auth): saves user to db

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,17 +1,11 @@
 import { Authenticator } from 'remix-auth'
 import { sessionStorage } from '~/services/session.server'
 import { GoogleStrategy, SocialsProvider } from 'remix-auth-socials'
+import prisma from '~/prisma/client.server'
 
 // Create an instance of the authenticator
 // It will take session storage as an input parameter and creates the user session on successful authentication
 export const authenticator = new Authenticator(sessionStorage)
-
-// callback function that will be invoked upon successful authentication from social provider
-async function handleSocialAuthCallback({ profile }: any) {
-  // create user in your db here
-  // profile object contains all the user data like image, displayName, id
-  return profile
-}
 
 // Configuring Google Strategy
 authenticator.use(
@@ -22,6 +16,19 @@ authenticator.use(
       scope: ['email', 'openid', 'profile'],
       callbackURL: `http://localhost:3000/auth/${SocialsProvider.GOOGLE}/callback`,
     },
-    handleSocialAuthCallback
+    async ({ profile }) => {
+      return prisma.user.upsert({
+        where: { id: profile.id },
+        update: {
+          displayName: profile.displayName,
+          email: profile.emails[0]?.value,
+        },
+        create: {
+          displayName: profile.displayName,
+          email: profile.emails[0]?.value,
+          id: profile.id,
+        },
+      })
+    }
   )
 )
